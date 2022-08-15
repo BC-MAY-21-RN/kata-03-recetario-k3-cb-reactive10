e8ab2224773fb6b6f2b3d87355bb98f00493c64f
Antonio de Jesus Hernandez Piña <tonyhdezpi@gmail.com>
2022-08-15 13:50:37 -0500

    Implement Card Component
    - Create card Component
    - Add styles for card Component
    
    co-authored-by: XimenaV26 <xvelasco1@ucol.mx>
    co-authored-by: featlast <jherrera892016@gmail.com>

--- recetario/src/Components/Card.jsx
+++ recetario/src/Components/Card.jsx
@@ -1,26 +1,50 @@
-import { View, Text,Image, StyleSheet } from 'react-native'
+import { View, Text,Image, StyleSheet, ScrollView } from 'react-native'
 import React from 'react'
+import { globalStyles } from '../Styles/GlobalStyle'
 
 export const Card = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Image
       style={styles.image}
       source={require('../assets/img/Taco.jpg')}
       />
-      <Text style={{color:'white'}}>Estamos en la CARD</Text>
+      <Text style={globalStyles.simpleTitle}>Treding</Text>
+      <Text style={globalStyles.productName}>Tacos</Text>
+
+      <View style={styles.containerScrollView}>
+        <Text style={globalStyles.description}>Ingredients
+          <Text> for 1 serving</Text>
+        </Text>
+        <ScrollView> 
+          <Text style={globalStyles.ingredients}><Text>Tortilla</Text> <Text>1 Tortilla</Text></Text>
+          <Text style={globalStyles.ingredients}>Carne</Text>
+          <Text style={globalStyles.ingredients}>Cilantro</Text>
+          <Text style={globalStyles.ingredients}>Cebolla</Text> 
+          <Text style={globalStyles.ingredients}>Salsa</Text>
+          <Text style={globalStyles.ingredients}>Limoncito</Text>
+        </ScrollView>
+      </View>
     </View>
+
   )
 }
 
 const styles = StyleSheet.create({
+  containerScrollView:{
+    flex: 1,
+    // flexDirection: 'column',
+    marginTop: 30
+  },
   container:{
-
+    width: '100%',
+    height: '100%'
   },
   image:{
-    width:120,
-    height: 100,
-    borderRadius:4
+    width: '100%',
+    height: 380,
+    borderRadius:4,
+    marginTop: 0
   }
 })
 
--- recetario/src/Styles/GlobalStyle.js
+++ recetario/src/Styles/GlobalStyle.js
@@ -11,16 +11,45 @@ export const globalStyles = StyleSheet.create({
   title: {
     fontSize: 32,
     fontWeight: "bold",
-    color: Colors.secondary,
+    color: Colors.secondary
   },
   simpleContainer: {
     flex: 1,
     backgroundColor: Colors.ligth,
   },
   simpleTitle: {
-    marginLeft: 10,
+    marginLeft: 30,
     fontSize: 32,
     fontWeight: "bold",
-    color: Colors.secondary,
+    color: Colors.ligth,
+    marginTop: -80
   },
+  productName: {
+    marginLeft: 30,
+    fontSize: 28,
+    fontWeight: "bold",
+    color: Colors.ligth,
+    marginTop: -5,
+  },
+  description: {
+    fontSize: 20,
+    color: Colors.ligth,
+    fontWeight: 'bold',
+    height: 35,
+    marginLeft: 20
+  },
+  ingredients: {
+    width: '100%',
+    fontSize: 20,
+    fontWeight: "400",
+    marginTop: 15,
+    color: Colors.ligth,
+    marginLeft: 20,
+    justifyContent: "space-between",
+    borderBottomColor: Colors.ligth,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }
 });
